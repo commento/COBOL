de843e8aaeb8d58e8984757f66db3404a25cbd09
Riccardo <riccardo@null.net>
2020-03-07 10:59:12 +0100

    add changes related to exercises on loops and condition names

--- Listing4-2.cob
+++ Listing4-2.cob
@@ -10,26 +10,34 @@ WORKING-STORAGE SECTION.
 01	Num2	PIC 9  VALUE 4.
 
 01  Operator PIC X VALUE SPACE.
-       88  MULT        VALUE "*".
-       88  DIV         VALUE "/".
-       88  SUMM        VALUE "+".
-       88  SUB         VALUE "-".
+       88  ValidOperator VALUE "*", "/", "+", "-".
+       88  ExitProgram   VALUE "s".
 
-01	Result  PIC 99 VALUE ZEROS.
+01	Result  PIC S99V99 VALUE ZEROS.
 
 PROCEDURE DIVISION.
+
+Begin.
+       DISPLAY "Enter an operator (*,/,-,+) - " WITH NO ADVANCING
+       ACCEPT Operator
+       PERFORM CalculateResult WITH TEST BEFORE UNTIL ExitProgram.
+       STOP RUN.
+
 CalculateResult.
        DISPLAY "Enter a single digit number - " WITH NO ADVANCING
        ACCEPT Num1
        DISPLAY "Enter a single digit number - " WITH NO ADVANCING
        ACCEPT Num2
-       DISPLAY "Enter an operator (*,/,-,+) - " WITH NO ADVANCING
-       ACCEPT Operator
-       EVALUATE TRUE
-           WHEN MULT  MULTIPLY Num1 BY Num2 GIVING Result
-           WHEN DIV   DIVIDE Num1 BY Num2 GIVING Result
-           WHEN SUMM  ADD Num1 TO Num2 GIVING Result
-           WHEN SUB   SUBTRACT Num2 FROM Num1 GIVING Result
+       EVALUATE Operator
+           WHEN "*"  MULTIPLY Num1 BY Num2 GIVING Result
+           WHEN "/"   DIVIDE Num1 BY Num2 GIVING Result
+           WHEN "+"  ADD Num1 TO Num2 GIVING Result
+           WHEN "-"   SUBTRACT Num2 FROM Num1 GIVING Result
        END-EVALUATE
+
+       IF ValidOperator
        DISPLAY "Result is = " Result
-       STOP RUN.
+       END-IF
+
+       DISPLAY "Enter an operator (*,/,-,+) - " WITH NO ADVANCING
+       ACCEPT Operator.
