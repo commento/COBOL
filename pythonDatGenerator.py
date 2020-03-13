5e83a84c3795db20db1f9d7c19dd5cba94e0fa5f
Riccardo <riccardo@null.net>
2020-03-13 13:35:17 +0100

    add exercise two-dimensional table
    three dimensional Listing11-3 has been abandoned when reached the report heading part -- too much code to write

--- pythonDatGenerator.py
+++ pythonDatGenerator.py
@@ -1,12 +1,21 @@
 import random
 
-f = open("BranchSales2.dat", "w")
+
+monthSalesDetails = True
+
+f = open("BranchSales3.dat", "w")
 recordId = 0
 while recordId < 60:
     string    = str(recordId).zfill(7)
     string   += str(random.randrange(1, 50, 1)).zfill(2)
-    string   += str(random.randrange(0, 9999999, 1)).zfill(7)
-    string   += str(99) + "\n"
+    if monthSalesDetails:
+        for i in range(12):
+            string += str(random.randrange(0, 99999, 1)).zfill(5)
+            string   += str(99)
+    else:
+        string   += str(random.randrange(0, 9999999, 1)).zfill(7)
+        string   += str(99)
+    string += "\n"
     f.write(string)
     recordId += 1
 
---
+++ twoDimensionalDataStructure.py
@@ -0,0 +1,7 @@
+
+# two dimensional table
+StatesTable = {'State' : [{}] * 50}
+
+for elem in StatesTable['State']:
+  elem['StateBranchCount'] = 0
+  elem['StateMonthSales'] =[0] * 12
