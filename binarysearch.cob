c91818b6eb2987929c7ad4ff6ace199cdbcaff84
Riccardo <riccardo@null.net>
2020-04-06 09:35:05 +0200

    add example from rosettacode

---
+++ binarysearch.cob
@@ -0,0 +1,17 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. binary-search.
+ 
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01  nums-area                           VALUE "01040612184356".
+    03  nums                            PIC 9(2)
+                                        OCCURS 7 TIMES
+                                        ASCENDING KEY nums
+                                        INDEXED BY nums-idx.
+PROCEDURE DIVISION.
+    SEARCH ALL nums
+        WHEN nums (nums-idx) = 43
+            DISPLAY "Found 4 at index " nums-idx
+    END-SEARCH
+    .
+END PROGRAM binary-search.
